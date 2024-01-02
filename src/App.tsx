f67cf94d0ff0202af49b12f7ca98225987967fee
Kiran <145902144+KiranGadhavi@users.noreply.github.com>
2024-01-02 13:15:30 +0000

    add stopwatch, coupon, toss app files

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,18 @@ const Dashboard =lazy(()=> import("./pages/Dashboard"));
 const Products =lazy(()=> import("./pages/Products"));
 const Customers =lazy(()=> import("./pages/Customers"));
 const Transaction =lazy(()=> import("./pages/Transaction"));
+
 const NewProduct =lazy(()=> import("./management/NewProduct"));
 const ProductManagement =lazy(()=> import("./management/ProductManagement"));
 const TransactionManagement =lazy(()=> import("./management/TransactionManagement"));
+
 const BarCharts =lazy(()=> import("./pages/charts/BarCharts"));
 const PieCharts =lazy(()=> import("./pages/charts/PieCharts"));
 const LineCharts =lazy(()=> import("./pages/charts/LineCharts"));
+
+const StopWatch =lazy(()=> import("./pages/apps/StopWatch"));
+const Coupon =lazy(()=> import("./pages/apps/Coupon"));
+const Toss =lazy(()=> import("./pages/apps/Toss"));
 
 function App() {
   
@@ -26,12 +32,18 @@ function App() {
         <Route path='/admin/products' element={<Products/>}/>
         <Route path='/admin/customers' element={<Customers/>}/>
         <Route path='/admin/transaction' element={<Transaction/>}/>
+       
         <Route path='/admin/product/new' element={<NewProduct/>}/>
         <Route path='/admin/product/:id' element={<ProductManagement/>}/>
         <Route path='/admin/transaction/:id' element={<TransactionManagement/>}/>
+       
         <Route path='/admin/chart/bar' element={<BarCharts/>}/>
         <Route path='/admin/chart/pie' element={<PieCharts/>}/>
         <Route path='/admin/chart/line' element={<LineCharts/>}/>
+
+        <Route path='/admin/app/StopWatch' element={<StopWatch/>}/>
+        <Route path='/admin/app/Coupon' element={<Coupon/>}/>
+        <Route path='/admin/app/Toss' element={<Toss/>}/>
       
       </Routes>
       </Suspense>
---
+++ src/pages/apps/Coupon.tsx
@@ -0,0 +1,93 @@
+import { FormEvent, useEffect, useState } from "react"
+import AdminSidebar from "../../components/AdminSidebar"
+const allLetters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const allNumbers = "1234567890"
+const allSymbols = "!@#%&^()_+*"
+
+const Coupon = () => {
+    const [size, setSize] =useState<number>(8);
+    const [prefix, setPrefix] =useState<string>("");
+    const [includeNumbers, setIncludeNumbers] =useState<boolean>(false);
+    const [includeCharacters, setIncludeCharacters] =useState<boolean>(false);
+    const [includeSymbols, setIncludeSymbols] =useState<boolean>(false);
+    const [isCopied, setIsCopied] =useState<boolean>(false);
+
+    const [coupon, setCoupon] = useState<string>()
+    const copyText = async (coupon:string)=>{
+
+       await window.navigator.clipboard.writeText(coupon);
+       setIsCopied(true);
+
+    }
+
+    const submitHandler = (e: FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        if(!includeNumbers && !includeCharacters && !includeSymbols) return
+        alert("Please Select One At Least")
+
+        let result: string = prefix || "";
+        const looplength: number= size -result.length;
+
+        for(let i = 0; i < looplength; i++){
+            let entireString: string = "";
+            if(includeCharacters)entireString+= allLetters;
+            if(includeNumbers)entireString+=allNumbers;
+            if(includeSymbols)entireString+=allSymbols;
+
+            const randonNum:number = ~~(Math.random()*entireString.length);
+        result += entireString[randonNum]
+        }
+        setCoupon(result);
+    };
+    useEffect(()=>{
+        setIsCopied(false);
+    },[coupon]);
+  return (
+    <div className="adminContainer">
+    <AdminSidebar/>
+    <main className="dashboard-app-container">
+        <h1>Coupon</h1>
+        <section>
+            <form className="coupon-form" onClick={submitHandler}>
+                <input type="text"
+                 placeholder="Text to include"
+                 value={prefix}
+                 onChange={(e) => setPrefix(e.target.value)}
+                 maxLength={size}
+                 ></input>
+
+                 <input type="text"
+                 placeholder="Text to include"
+                 value={size}
+                 onChange={(e) => setSize(Number(e.target.value))}
+                 min={8}
+                 max={25}
+                 ></input>
+            <fieldset>
+                <legend>Include</legend>
+                <input type="checkbox"
+                 checked={includeNumbers}
+                 onChange={() => setIncludeNumbers((prev)=> !prev)}
+                 />
+                 <span>Numbers</span>
+                 <input type="checkbox"
+                 checked={includeCharacters}
+                 onChange={() => setIncludeCharacters((prev)=> !prev)}
+                 />
+                 <span>Characters</span>
+                 <input type="checkbox"
+                 checked={includeSymbols}
+                 onChange={() => setIncludeSymbols((prev)=> !prev)}
+                 />
+                 <span>Symbols</span>
+            </fieldset>
+            <button type="submit">Generate</button>
+            </form>
+            {coupon && <code>{coupon} <span onClick={() => copyText(coupon)}>{isCopied?"Copied":"Copy"}</span></code>}
+        </section>
+        </main>
+    </div>
+  )
+}
+
+export default Coupon
